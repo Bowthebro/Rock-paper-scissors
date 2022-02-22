42e4b95e47f1af6b87ba911f7710c49118cd85a3
Bowthebro <85522775+Bowthebro@users.noreply.github.com>
2022-02-23 10:56:45 +1300

    Create Rockpaperscissors.py
    ran out of time for sdl so this is what i have rn

---
+++ Rockpaperscissors.py
@@ -0,0 +1,13 @@
+import random
+print("would you like to play?")
+yes_no = input()
+
+def game():
+    print("play again")
+    print("rock paper scissors")
+    user_input = input()
+    robot_input = random.randint(1, 3)
+    if robot
+
+while yes_no == "y":
+    game()
